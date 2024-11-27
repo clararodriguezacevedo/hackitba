791849215fc82b13e08fac77b7328c3e1ffeca7a
Clara Rodriguez Acevedo <78107786+clararodriguezacevedo@users.noreply.github.com>
2024-11-27 15:47:46 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+
+	import '../lib/i18n/index';
+	import { locale } from 'svelte-i18n';
+	locale.set('en');
+</script>
+
+{@render children()}
